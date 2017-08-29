111fa62dba383551f2c59b485772b5b60303e88b
Mikkel Plagborg-Moller <mikkel.p.m@gmail.com>
2017-08-29 16:55:37 -0400

    Slight modification of SimInference class for consistency
    Added description of SimInference class at top of code (consistent with other code), changed "Vartheta" to "Sigma" for consistency with paper's notation, a few other cosmetic changes

--- SimInferenceClass/SimInference.m
+++ SimInferenceClass/SimInference.m
@@ -1,12 +1,20 @@
 classdef SimInference
-    %This Matlab class provides some functions to implement simultaneous
-    %confidence bands in different problems (be it time series or cross-sectional)
+% -----------------------------------------------------------------------------
+% This Matlab class provides generic functions to implement simultaneous
+% confidence bands in different problems (be it time series or cross-sectional)
+%
+% This version: August 29, 2017
+% J. L. Montiel Olea & M. Plagborg-Moller
+% ------------------------------------------------------------------------------
     
     properties
     end
     
     methods(Static=true)
+        
+        
     %% 1)Transformation of Interest:    
+    
         function  theta=theta(mu,h)
             %This function computes the transformation of interest
             %(theta in the notation used by Montiel-Olea and Plagborg-Moller)
@@ -29,8 +37,10 @@ classdef SimInference
             end                        
         end
         
+        
     %% 2)Standard Errors based on resampling:
-        function [thetadraws,rs_stderrors,Vartheta]=rs_stderrors(mudraws,h)
+    
+        function [thetadraws,rs_stderrors,Sigma]=rs_stderrors(mudraws,h)
             %This function estimates the covariance matrix of 
             %thetahat based on a vector of mdraws. The name of the function
             %"rs_stderrors" is an abbreviation for "resampled standard errors"
@@ -48,9 +58,12 @@ classdef SimInference
             %------
             thetadraws=SimInference.theta(mudraws,h);
             rs_stderrors=std(thetadraws,0,1);
-            Vartheta=cov(thetadraws');
+            Sigma=cov(thetadraws');
         end
-    %% 3)Typical Critical Values for Rectangular Bands:
+        
+        
+    %% 3)Critical Values for Rectangular Bands:
+    
         function [Pwise,Sidak,Bonferroni,thetaproj,muproj]...
             =critvalues(confidence,p,k)
         %This function reports typical critical values for 
@@ -60,27 +73,28 @@ classdef SimInference
         Bonferroni=norminv(1-((1-confidence)/(2*(k))),0,1);
         thetaproj=chi2inv(confidence,k)^.5;
         muproj=chi2inv(confidence,p)^.5;
-%         if k > p
-%             disp('Note that the thetaproj critical value is not valid if k>p.')
-%             disp('(See Inoue and Kilian (2016) in JOE)')
-%         else
-%         end
+        end
         
-        end
-        %% 4)(Plug-in) Supt Critical Value for Rectangular Bands:
-        function [suptplugin]=suptcritval_plugin(confidence,Vartheta,I)
-            %This function reporst the supt critical value, based on MVN
-            %draws.
-            d=diag(Vartheta).^(.5);   % Extracts the diagonal of Vartheta
+        
+        %% 4)(Plug-in) Sup-t Critical Value for Rectangular Bands:
+        
+        function [suptplugin]=suptcritval_plugin(confidence,Sigma,I)
+            %This function reports the sup-t critical value, based on
+            %multivariate normal draws.
+            d=diag(Sigma).^(.5);   % Extracts the diagonal of Vartheta
             d_nonzero = (d>eps); % Elements with nonzero variance
-            Corrmat=bsxfun(@ldivide, d(d_nonzero), bsxfun(@rdivide, Vartheta(d_nonzero,d_nonzero), d(d_nonzero)')); %Matrix of Correlations
+            Corrmat=bsxfun(@ldivide, d(d_nonzero), bsxfun(@rdivide, Sigma(d_nonzero,d_nonzero), d(d_nonzero)')); %Matrix of Correlations
+            % Compute square root using eigendecomposition
             [Corrmat_V,Corrmat_D] = eig(Corrmat);
-            Corrmat_chol = sqrt(Corrmat_D)*Corrmat_V';
+            Corrmat_sqrt = sqrt(Corrmat_D)*Corrmat_V';
             
-            t=abs(randn(I,size(Corrmat_chol,1))*Corrmat_chol); %limit of tstats
-            suptplugin=quantile(max(t,[],2),confidence,1);                 
+            t=abs(randn(I,size(Corrmat_sqrt,1))*Corrmat_sqrt); %limit of tstats
+            suptplugin=quantile(max(t,[],2),confidence,1);
         end
+        
+        
         %% 5) 2-D plots of Rectangular Bands based on Theoretical C-values
+        
         function [Rband]=Rbands(cval,stderror,thetahat)
             %INPUT
             %-----
@@ -96,7 +110,10 @@ classdef SimInference
             Rband(1,:)=thetahat-(cval*stderror);
             Rband(2,:)=thetahat+(cval*stderror);
         end
+        
+        
         %% 6) "Credibility" of rectangular bands based on resampled values of mu
+        
         function [cred]=credibility_Rbands(thetadraws,Rband)
             %INPUT
             %------
@@ -113,13 +130,17 @@ classdef SimInference
             cred=mean(aux1==k);
         end
         
-        %% 7) Confidence Band based on the quantiles of mudraws
+        
+        %% 7) Equal-tailed Confidence Band based on the quantiles of mudraws
+        
         function [quantileRband]=quantile_Rbands(thetadraws,confidence)
         quantileRband(1,:)=quantile(thetadraws,(1-confidence)/2,1);
         quantileRband(2,:)=quantile(thetadraws,1-((1-confidence)/2),1);
         end
         
+        
         %% 8) "Calibrated" Confidence Band
+        
         function [Calibrated,x,Cred]=calibrated_Rbands(thetadraws,confidence)
             k=size(thetadraws,2);
             f=@(x) SimInference.credibility_Rbands(thetadraws,SimInference.quantile_Rbands(thetadraws,max(min(x,1),0)))-confidence;
@@ -129,7 +150,9 @@ classdef SimInference
             Cred=fval+confidence;
         end
         
-        %% 9) Wrapper function for returning any number of desired plug-in bands
+        
+        %% 9) Wrapper function for returning desired plug-in bands
+        
         function the_bands = bands_plugin(thetahat, Vartheta, p, band_list, I, confidence)
             % Returns a cell array of 2D bands
             % band_list is a cell array containing any combination of: 'Pwise', 'Sidak', 'supt', 'Bonferroni', 'thetaproj', 'muproj'
