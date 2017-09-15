a088a74e124de264e6aa482e2d46381640e83a9b
jm4474 <montiel.olea@gmail.com>
2017-09-15 11:49:52 -0400

    "Minor edits in format to make it similar to gk_ivdata"

--- Gertler_Karadi_application/Scripts/gk_VARdata.m
+++ Gertler_Karadi_application/Scripts/gk_VARdata.m
@@ -1,12 +1,11 @@
 %% Load VAR data
 
-% Data settings
-var_file = '../Data/VAR_data.csv';
-var_cols = [3 4 6 11]; % Column numbers of VAR variables
+var_file = '../Data/VAR_data.csv';      % Data settings
 
-% Read from file
-var_dat = importdata(var_file);
+var_cols = [3 4 6 11];                  % Column numbers of VAR variables
+ 
+var_dat = importdata(var_file);         % Read from file
 
-% Define variable
-Y = var_dat.data(:,var_cols);
-d = size(Y,2);
+Y       = var_dat.data(:,var_cols);     % Define variables
+
+d       = size(Y,2);
